19063101490cd7ee7d8d61cdb35e77df70cf9bf3
Andrea <andreabejaraano@gmail.com>
2023-04-29 09:27:15 -0600

    Agregar archivos de proyecto.

---
+++ proyectodosJarodBolanos/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectodosJarodBolanos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public struct Datos
+        {
+            public string jugadoruno;
+            public string jugadordos;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            button1.Enabled = false;
+            //MessageBox.Show("MUESTRA MENSAJE");
+            textBox1.Enabled = true;
+            textBox1.Visible = true;
+            label1.Visible = true;
+            textBox2.Enabled = true;
+            textBox2.Visible = true;
+            label2.Visible = true;
+            button3.Enabled = true;
+            button3.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            MessageBox.Show("El jugador contrario será la computadora");
+            textBox1.Enabled = true;
+            textBox1.Visible = true;
+            label1.Visible = true;
+            button3.Enabled = true;
+            button3.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Enabled == true && (textBox2.Text == "Computadora" || textBox2.Text == "computadora"|| textBox1.Text == "Computadora" || textBox1.Text == "computadora"))
+            {
+                MessageBox.Show("Debe ingresar algun nombre, no Computadora");
+            }
+            else
+            {
+
+            Datos jugadores;
+            jugadores.jugadoruno = textBox1.Text;
+            jugadores.jugadordos = textBox2.Text;
+
+            string jugadorunotex="", jugadordostex="";
+            jugadorunotex = textBox1.Text;
+            jugadordostex = textBox2.Text;
+            if (jugadorunotex==""||jugadordostex=="")
+            {
+                MessageBox.Show("Escriba el nombre del jugador");
+            }
+            else
+            {    //METODO , CUANDO SE OCULTAN LAS VENTANAS MIENTRAS OTRAS ESTAN ABIERTAS Y SE REACOMODARON LOS BOTONES
+                this.Hide();
+                gamejugadores segundoform = new gamejugadores(jugadores);
+                // SE DEFINE CLASE PARA MOSTRARLA, ES EQUIVALENTE A ESTABLECER LA VISIBLE PROPIEDAD
+                    segundoform.Show();
+            }
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {// CIERRA EL FORMS PRINCIPAL
+            this.Close();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ proyectodosJarodBolanos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("proyectodosJarodBolanos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("proyectodosJarodBolanos")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e4e303d2-b514-4459-b148-2c37af836f89")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
